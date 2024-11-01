6bc973eec59d4731d90f24965642b083637e955d
Willem van Heemstra <wvanheemstra@icloud.com>
2024-11-01 11:19:02 +0000

    Update

--- threagile-builder/src/threagile_builder/utils/app_utils.py
+++ threagile-builder/src/threagile_builder/utils/app_utils.py
@@ -1,11 +1,24 @@
 # threagile-builder/scr/threagile_builder/utils/app_utils.py
+import logging
+
+from apiflask import APIFlask
 from config import Config
 from flask import Flask
 
 
 def create_app(config=Config):
-    app = Flask(__name__)
+    app = APIFlask(
+        __name__,
+        static_url_path="/static",
+        static_folder="../static",
+        template_folder="../templates",
+    )
     app.config.from_object(config)
+
+    # Configure logging
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )    
 
     @app.route("/")
     def index():
