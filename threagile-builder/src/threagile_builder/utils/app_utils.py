8713aee0d03b87e1132986e7d63596285489b587
Willem van Heemstra <wvanheemstra@icloud.com>
2024-11-01 11:13:51 +0000

    Update

--- threagile-builder/src/threagile_builder/utils/app_utils.py
+++ threagile-builder/src/threagile_builder/utils/app_utils.py
@@ -1,9 +1,11 @@
 # threagile-builder/scr/threagile_builder/utils/app_utils.py
+from config import Config
 from flask import Flask
 
 
-def create_app():
+def create_app(config=Config):
     app = Flask(__name__)
+    app.config.from_object(config)
 
     @app.route("/")
     def index():
