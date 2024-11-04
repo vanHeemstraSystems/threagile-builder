6907647cffefb49f90d1f342683f1d1797ff5712
Willem van Heemstra <wvanheemstra@users.noreply.github.com>
2024-11-04 13:31:51 +0100

    Update __init__.py

--- threagile-builder/src/threagile_builder/routes/__init__.py
+++ threagile-builder/src/threagile_builder/routes/__init__.py
@@ -1,3 +1,3 @@
-# threagile-builder/scr/threagile_builder/routes/__init__.py
+# threagile-builder/src/threagile_builder/routes/__init__.py
 from .build_routes import build_bp
 from .main_routes import main_bp
