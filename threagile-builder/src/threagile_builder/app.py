62260ad4200215f9da642d9a64dde215972c562e
Willem van Heemstra <wvanheemstra@users.noreply.github.com>
2024-11-04 13:24:09 +0100

    Update app.py

--- threagile-builder/src/threagile_builder/app.py
+++ threagile-builder/src/threagile_builder/app.py
@@ -2,16 +2,6 @@
 import os
 from utils import create_app
 
-# from flask import *
-# app = Flask(__name__)
-
-# @app.route("/")
-# def index():
-#   return "<h1>Hello World</h1>"
-
-# if __name__ == "__main__":
-#   app.run(host="0.0.0.0", port=8080, debug=False)
-
 app = create_app()
 
 if __name__ == "__main__":
