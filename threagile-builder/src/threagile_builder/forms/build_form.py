1b8c71072887fa85e7951478186a09673ceeec17
Willem van Heemstra <wvanheemstra@users.noreply.github.com>
2024-11-08 11:45:50 +0000

    Update

--- threagile-builder/src/threagile_builder/forms/build_form.py
+++ threagile-builder/src/threagile_builder/forms/build_form.py
@@ -1,9 +1,10 @@
 # threagile-builder/src/threagile_builder/forms/build_form_.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class BuildForm(FlaskForm):
     title = StringField("Build Title", validators=[DataRequired()])
+    description = TextAreaField('Description')
     submit = SubmitField("Create Build")
