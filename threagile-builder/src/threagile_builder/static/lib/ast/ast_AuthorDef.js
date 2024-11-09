d1e583b4b2ca3bb5ee7867c85a8abd90398dd9f1
Willem van Heemstra <wvanheemstra@users.noreply.github.com>
2024-11-09 13:39:25 +0000

    Update

---
+++ threagile-builder/src/threagile_builder/static/lib/ast/ast_AuthorDef.js
@@ -0,0 +1,17 @@
+Blockly.Blocks['ast_AuthorDef'] = {
+    init: function () { },
+    // TODO: Not mutable currently
+    updateShape_: function () { },
+    /**
+     * Create XML to represent the (non-editable) name and arguments.
+     * @return {!Element} XML storage element.
+     * @this Blockly.Block
+     */
+    mutationToDom: function () { },
+    /**
+     * Parse XML to restore the (non-editable) name and parameters.
+     * @param {!Element} xmlElement XML storage element.
+     * @this Blockly.Block
+     */
+    domToMutation: function (xmlElement) { },
+};
